376b7fe3f25c510fde93c7719d976620576ab884
mithun <mithun@192.168.0.201>
2022-12-10 23:58:52 +0530

    changes made

--- insurance-client/src/main/java/com/springboot/insurance/client/InsuranceClientApplication.java
+++ insurance-client/src/main/java/com/springboot/insurance/client/InsuranceClientApplication.java
@@ -3,6 +3,7 @@ package com.springboot.insurance.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
--- insurance-client/src/main/java/com/springboot/insurance/client/controller/InsuranceClientController.java
+++ insurance-client/src/main/java/com/springboot/insurance/client/controller/InsuranceClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("insurance-client")
+@RefreshScope
 public class InsuranceClientController {
 
 	@Autowired
