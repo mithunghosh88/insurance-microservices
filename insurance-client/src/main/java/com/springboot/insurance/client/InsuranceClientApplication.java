e73c690f84879d192725004b08bcfc71f4012c69
mithun <mithun@192.168.0.201>
2022-12-11 00:01:23 +0530

    change made

--- insurance-client/src/main/java/com/springboot/insurance/client/InsuranceClientApplication.java
+++ insurance-client/src/main/java/com/springboot/insurance/client/InsuranceClientApplication.java
@@ -3,7 +3,6 @@ package com.springboot.insurance.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
