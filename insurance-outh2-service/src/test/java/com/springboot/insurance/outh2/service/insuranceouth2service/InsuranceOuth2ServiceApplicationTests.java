68b1e89c01980e6887a4dee3341de204b360da7a
mithun <mithun@192.168.0.103>
2022-12-11 22:56:46 +0530

    changes made

---
+++ insurance-outh2-service/src/test/java/com/springboot/insurance/outh2/service/insuranceouth2service/InsuranceOuth2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.insurance.outh2.service.insuranceouth2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceOuth2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
